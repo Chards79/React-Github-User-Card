e8b80f0bf6667c2620a97ae18798b95f5c671f94
Eric Richards <esholundr@yahoo.com>
2019-10-01 16:54:35 -0600

    Added necessary files

---
+++ my-github-user-card-app/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends React.Component {
+  state = {
+    myGithub: 'Eric',
+
+  }
+
+
+
+  return(
+    <div className = "App" >
+      <header className="App-header">
+
+        Learn React
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
